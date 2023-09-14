769f79c1420f7b63861e73d806459725a96866d7
odairsp <odair.sp@hotmail.com>
2023-09-14 13:26:14 -0300

    att

--- mission1/pt/terraform/tcb_aws_storage.tf
+++ mission1/pt/terraform/tcb_aws_storage.tf
@@ -1,6 +1,6 @@
 # Linhas para criar o bucket S3 na AWS
 
 resource "aws_s3_bucket" "b" {
-  bucket = "luxxy-covid-testing-system-pdf-pt-xxxx"
+  bucket = "luxxy-covid-testing-system-pdf-pt-ohf78"
   acl = "private"
 }
